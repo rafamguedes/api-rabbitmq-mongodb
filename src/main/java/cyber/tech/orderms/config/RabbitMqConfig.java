4eb2088c6b94958dfc9d83a1a8bdcb496a766115
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-12-25 19:23:04 -0300

    config rabbitMQ and jackson serializable

---
+++ src/main/java/cyber/tech/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,22 @@
+package cyber.tech.orderms.config;
+
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+  public static final String ORDER_QUEUE = "order.queue";
+
+  @Bean
+  public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public Declarable orderQueue() {
+    return new Queue(ORDER_QUEUE);
+  }
+}
